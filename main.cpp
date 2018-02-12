fed798110fed3bedb8f93fba673c4f7cf9dfaf18
Jesse <jmkeller4@alaska.edu>
2018-02-11 17:49:26 -0900

    Initial Board Tiling uploaded

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 int main()
 {
 	// create the window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My window",sf::Style::Close);
+	sf::Texture texture;
+	if (!texture.loadFromFile("Tile_Ocean.jpg"))
+		std::cout << "File not found" << std::endl;
+	else
+		std::cout << "file found" << std::endl;
+		
+	sf::Sprite sprite;
+	float scale = .05;
+	sprite.setScale(sf::Vector2f(scale,scale));
+	sprite.setTexture(texture);
+	sf::CircleShape circ(50);
+
 
 	// run the program as long as the window is open
 	while (window.isOpen())
@@ -19,6 +37,17 @@ int main()
 
 		// clear the window with black color
 		window.clear(sf::Color::Black);
+
+		for (int i = 0;i < 10;i++)
+		{
+			for (int j = 0;j < 10;j++)
+			{
+				sprite.setPosition(sf::Vector2f((float)((70)*i), (float)(70*j) ));
+				window.draw(sprite);
+
+			}
+		}
+		//window.draw(sprite);
 
 		// draw everything here...
 		// window.draw(...);
