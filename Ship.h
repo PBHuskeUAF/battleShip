e9df7dc852d94c0e402ef230a690d73b4b2fe60a
space314 <jmkeller4@alaska.edu>
2018-02-18 20:14:39 -0900

    more fixing git

--- Ship.h
+++ Ship.h
@@ -5,6 +5,7 @@
 #include<utility>
 #include <random> 
 #include <functional>
+#include <SFML\Graphics.hpp>
 using std::pair;
 using std::vector;
 
@@ -182,5 +183,20 @@ pair <int, int> place_Ship()
 	return coord;
 }
 
+};
+
+class GenericTestShip
+{
+public:
+	enum DIRECTION { VERT, HORIZ };
+	GenericTestShip();
+	GenericTestShip(int size, DIRECTION dir, std::vector<float>& position);
+	void render( sf::RenderWindow&, sf::Vector2f &);
+private:
+	std::vector<float> m_Position;
+	DIRECTION m_dir;
+	int m_size;
+	sf::Texture m_texture;
+	sf::RectangleShape m_shape;
 };
 #endif
